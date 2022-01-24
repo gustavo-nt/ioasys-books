143257045648a24954cbeb8419b85c440714e4d2
gustavo-nt <gustavont.dev@gmail.com>
2022-01-23 21:14:42 -0300

    Update SEO

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,6 +12,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <meta charSet="utf-8" />
         <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+
         <meta name="google" content="notranslate" />
         <meta name="robots" content="noindex,nofollow" />
         <meta name="google" content="nositelinkssearchbox" />
@@ -19,9 +20,14 @@ function MyApp({ Component, pageProps }: AppProps) {
 
         <meta name="theme-color" content="#B22E6F" />
         <meta name="author" content="Gustavo Nunes Teixeira" />
+        <meta name="description" content="ioasys Books" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-        <link rel="icon" href="./favicon.png" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="ioasys Books" />
+        <meta property="og:description" content="ioasys Books" />
+
+        <link rel="shortcut icon" href="./favicon.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
